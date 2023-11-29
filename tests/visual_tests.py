b0294f5528c176fe736bbafb60d2e7cd04755851
Heitor Polidoro <heitor.polidoro@gmail.com>
2023-11-29 14:24:55 -0300

    visual tests

--- tests/visual_tests.py
+++ tests/visual_tests.py
@@ -2,12 +2,13 @@ import sys
 sys.path.append('.')
 from github_actions_utils.log import debug, notice
 
+file = "github_actions_utils/log.py"
 
 debug("This is a debug")
 notice("This is just a notice")
-notice("This is a notice with title and file", title="Nice Title", file="log.py")
-notice("This is a notice with line", line=2)
-notice("This is a notice with line and endLine", line=2, end_line=4)
-notice("This is a notice with col", line=1, col=2)
-notice("This is a notice with col and endCol", line=4, end_line=5)
-notice("This is a notice with col and endCol", line=4, col=2, end_col=5)
+notice("This is a notice with title and file", title="Nice Title", file=file)
+notice("This is a notice with line", line=2, file=file)
+notice("This is a notice with line and endLine", line=2, end_line=4, file=file)
+notice("This is a notice with col", line=1, col=2, file=file)
+notice("This is a notice with col and endCol", line=4, end_line=5, file=file)
+notice("This is a notice with col and endCol", line=4, col=2, end_col=5, file=file)
