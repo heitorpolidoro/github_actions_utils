75be5c737387639c00891af0acf6801df6406637
Heitor Polidoro <heitor.polidoro@gmail.com>
2023-11-29 17:53:47 -0300

    final touches 2

--- tests/visual_tests.py
+++ tests/visual_tests.py
@@ -8,13 +8,13 @@ file = "tests/visual_tests.py"
 debug("This is a debug")
 
 notice_start_line = 10
-notice("This is just a notice")
-notice("This is a notice", title="Nice Title", file=file)
+notice("This is a notice")
+notice("This is a file notice with title", title="Nice Title", file=file)
 
 warning_start_line = 10
-warning("This is just a warning")
-warning("This is a warning", title="Nice Title", file=file)
+warning("This is a warning")
+warning("This is a file warning with title", title="Nice Title", file=file)
 
 error_start_line = 10
-error("This is just a error")
-error("This is a error", title="Nice Title", file=file)
+error("This is a error")
+error("This is a file error with title", title="Nice Title", file=file)
