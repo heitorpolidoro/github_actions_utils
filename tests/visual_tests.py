a943d70e835589b1e0ca29aa9e9464743e3382bb
Heitor Polidoro <heitor.polidoro@gmail.com>
2023-11-30 15:22:15 -0300

    linter test

--- tests/visual_tests.py
+++ tests/visual_tests.py
@@ -12,6 +12,9 @@ file = "tests/visual_tests.py"
 
 debug("This is a debug")
 
+
+
+
 notice("This is a notice")
 notice("This is a file notice with title", title="Nice Title", file=file)
 
@@ -19,7 +22,7 @@ warning("This is a warning")
 warning("This is a file warning with title", title="Nice Title", file=file)
 
 error("This is a error")
-error("This is a file error with title", title="Nice Title", file=file)
+error("This is a file error with title", title="Nice Title", file = file)
 
 with group("Group title"):
     print("logs inside group")
